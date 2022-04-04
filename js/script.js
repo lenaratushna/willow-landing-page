9adb2009f8c2aaafc06e89ba1ca2e6c7a8fc5be5
Lena <olena.ratushna21@gmail.com>
2022-04-04 19:36:37 +0300

    added projects, skills, awards, reviews and blog sections

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    speed: 1200,
+    loop: true,
+	navigation: {
+		nextEl: '.reviews__slider-button-next',
+		prevEl: '.reviews__slider-button-prev',
+	},
+});
